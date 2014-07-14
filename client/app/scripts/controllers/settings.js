df788bf76572a23b8e9eb423624a072a918f3de0
Gorlan Yuen <gorlan@wix.com>
2014-07-13 17:23:34 -0700

    Display changes after first review

--- client/app/scripts/controllers/settings.js
+++ client/app/scripts/controllers/settings.js
@@ -4,4 +4,22 @@ angular.module('3dViewer')
   .controller('SettingsCtrl', function ($scope, $wix) {
   	$wix.UI.initialize();
 
+  	$scope.modelUploaded = true;
+
+  	$scope.lightingOptions= false;
+
+  	$scope.modelColors = false;
+
+  	$scope.enableLightingOptions = function() {
+  		$scope.lightingOptions = true;
+  	}
+
+  	$scope.disableLightingOptions = function() {
+  		$scope.lightingOptions = false;
+  	}
+
+  	$scope.enableModelColors = function() {
+  		$scope.modelColors = true;
+  	}
+
 });
